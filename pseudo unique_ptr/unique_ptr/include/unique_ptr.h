bcb879834090f2b18b1d5e126e210fd56554a8ba
NikitaBukhta <nikita.bukhta.dev@gmail.com>
2022-06-18 16:44:05 +0300

    own unique ptr added

---
+++ pseudo unique_ptr/unique_ptr/include/unique_ptr.h
@@ -0,0 +1,30 @@
+#ifndef UNIQUE_PTR_H
+#define UNIQUE_PTR_H
+
+namespace smart_ptr{
+    template <typename T>
+    class UniquePtr{
+    private:
+        T *_ptr;
+    public:
+        UniquePtr(T *ptr = nullptr);
+
+        ~UniquePtr(void);
+
+        UniquePtr(const UniquePtr<T> &other) = delete;
+
+        UniquePtr(UniquePtr<T> &&other);
+
+        void operator= (const UniquePtr<T> &other) = delete;
+
+        void operator= ( UniquePtr<T> &&other);
+
+        T& operator* (void) const noexcept;
+
+        T* get(void) const noexcept;
+
+        void reset(T *ptr = nullptr) noexcept;
+    };
+}
+
+#endif // !UNIQUE_PTR_H
---
+++ pseudo unique_ptr/unit_testing/test_smart_pointers.cpp
@@ -0,0 +1,38 @@
+#include "gtest/gtest.h"    // google tests;
+#include "unique_ptr"
+
+using namespace smart_ptr;
+
+const int CHECKED_NUMBER = 10;
+
+TEST(UniquePtrTest, TestForReset){
+    UniquePtr<int> unique(new int(CHECKED_NUMBER));
+    unique.reset();
+    ASSERT_EQ(unique.get(), nullptr);
+}
+
+TEST(UniquePtrTest, TestForDereference){
+    UniquePtr<int> unique(new int(CHECKED_NUMBER));
+
+    ASSERT_EQ(*unique, CHECKED_NUMBER);
+}
+
+TEST(UniquePtrTest, TestForMoving){
+    UniquePtr<int> a (new int(CHECKED_NUMBER));
+    auto b (std::move(a));
+
+    ASSERT_EQ(a.get(), nullptr);
+    ASSERT_EQ(*b, CHECKED_NUMBER);
+
+    UniquePtr<int> c;
+    c = std::move(b);
+
+    ASSERT_EQ(b.get(), nullptr);
+    ASSERT_EQ(*c, CHECKED_NUMBER);
+}
+
+int main(int argc, char **argv){
+    testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
